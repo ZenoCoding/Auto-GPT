294d45c53fd442de0c42d1f54447601ee8381119
ZenoCoding <51522505+ZenoCoding@users.noreply.github.com>
2023-04-21 14:24:08 -0700

    twit update

--- autogpt/commands/twitter.py
+++ autogpt/commands/twitter.py
@@ -8,6 +8,7 @@ from autogpt.commands.command import command
 
 load_dotenv()
 
+<<<<<<< Updated upstream
 
 @command(
     "send_tweet",
@@ -25,20 +26,30 @@ def send_tweet(tweet_text: str) -> str:
       Returns:
           A result from sending the tweet.
     """
+=======
+def send_tweet(tweet_text):
+>>>>>>> Stashed changes
     consumer_key = os.environ.get("TW_CONSUMER_KEY")
     consumer_secret = os.environ.get("TW_CONSUMER_SECRET")
     access_token = os.environ.get("TW_ACCESS_TOKEN")
     access_token_secret = os.environ.get("TW_ACCESS_TOKEN_SECRET")
+    bearer_token = os.environ.get("TW_BEARER_TOKEN")
     # Authenticate to Twitter
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-
-    # Create API object
-    api = tweepy.API(auth)
-
+    client = tweepy.Client(bearer_token=bearer_token,
+                    consumer_key=consumer_key,
+                    consumer_secret=consumer_secret,
+                    access_token=access_token,
+                    access_token_secret=access_token_secret)
     # Send tweet
     try:
+<<<<<<< Updated upstream
         api.update_status(tweet_text)
         return "Tweet sent successfully!"
     except tweepy.TweepyException as e:
         return f"Error sending tweet: {e.reason}"
+=======
+        client.create_tweet(text=tweet_text)
+        print("Tweet sent successfully!")
+    except tweepy.TweepyException as e:
+        print("Error sending tweet: {}".format(e.reason))
+>>>>>>> Stashed changes
